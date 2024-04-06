17425e1f0b25a2a1384f3470ca8fbef668a18b71
uche2064 <godswilllek02@gmail.com>
2024-04-06 18:35:50 +0000

    Finished my first project with tailwindcss

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector("#menu-btn")
+const nav = document.querySelector("#menu")
+
+btn.addEventListener("click", function (e) {
+    btn.classList.toggle("open")
+    nav.classList.toggle("flex")
+    nav.classList.toggle("hidden")
+})
